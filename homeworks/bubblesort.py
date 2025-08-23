97f432386e716c3b06117e9f5f99eb692eff93e9
Admin <laburr@yandex.ru>
2025-08-24 00:40:03 +0300

    new commit

---
+++ homeworks/bubblesort.py
@@ -0,0 +1,13 @@
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for number in range(0, n - i - 1):
+            if arr[number] > arr[number + 1]:
+                arr[number], arr[number + 1] = arr[number + 1],  arr[number]
+                print(my_list)
+
+
+my_list = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(my_list)
+print("Отсортированный список:", my_list)
