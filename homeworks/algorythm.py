45f0c6bc006be2c6e30d1801b57bb6cad671487e
Admin <laburr@yandex.ru>
2025-09-08 22:20:51 +0300

    new commit

---
+++ homeworks/algorythm.py
@@ -0,0 +1,42 @@
+
+
+class MyDict:
+
+    def __init__(self):
+        self._data = {}
+
+    def __getitem__(self, key):
+        return self._data.get(key, None)
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        if key in self._data:
+            del self._data[key]
+
+    def keys(self):
+        return list(self._data.keys())
+
+    def values(self):
+        return list(self._data.values())
+
+    def items(self):
+        return list(self._data.items())
+
+    def __str__(self):
+        return str(self._data)
+
+    def __contains__(self, key):
+        return key in self._data
+
+
+
+my_dict = MyDict()
+my_dict['name'] = 'Alice'
+my_dict['age'] = 30
+print(my_dict['name'])  # Вернет 'Alice'
+print('city' in my_dict)  # Вернет False
+del my_dict['age']
+print(my_dict.keys())  # Вернет ['name']
+print(my_dict.values())  # Вернет ['Alice']
